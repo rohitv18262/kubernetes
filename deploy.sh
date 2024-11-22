a9fb232154f6f9ec80182ea96dfbce167c0ef233
rohit.verma1@tothenew.com <rohit.verma1@tothenew.com>
2024-11-22 22:04:33 +0530

    final image and readme.md

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ kubectl apply -f applications/app1/service.yaml
 kubectl apply -f applications/app2/deployment.yaml 
 kubectl apply -f applications/app2/service.yaml 
 kubectl apply -f ingress.yaml -n default
-# kubectl apply -f configmap.yaml -n ingress-nginx
 kubectl delete configmap ingress-nginx-controller -n ingress-nginx
 kubectl apply -f configmap.yaml -n ingress-nginx
 kubectl rollout restart deployment ingress-nginx-controller -n ingress-nginx
--- destroy.sh
+++ destroy.sh
@@ -2,4 +2,3 @@
 kubectl delete deployment --all
 kubectl delete service --all
 kubectl delete ingress app-ingress -n default
-# kubectl delete configmap mycustom-configmap -n ingress-nginx
