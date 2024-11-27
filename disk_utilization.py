2f91a44944b16a3ab95abadcb34ca3cb37ba83b7
rohit.verma1@tothenew.com <rohit.verma1@tothenew.com>
2024-11-27 21:50:28 +0530

    increased replicas to 3

--- disk_utilization.py
+++ disk_utilization.py
@@ -2,7 +2,10 @@
 import subprocess
 import csv
 import pandas as pd
+import json
+import requests
 cmd = ['df', '-h']
+slack_channel = 'https://hooks.slack.com/services/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'
 
 lsProcess = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 output , error= lsProcess.communicate()
@@ -30,11 +33,20 @@ df = pd.read_csv("df_output.csv")
 
 df['Use%'] = df['Use%'].str.rstrip('%').astype(int)
 
-result = df[df['Use%'] >= 18]
+result = df[df['Use%'] >= 75]
 
 if not result.empty:
     print(result)
+    print(type(result))
+    result_string = result.to_string()
+    slack_payload = {'type': 'mrkdwn', 'text': result_string}
+    response = requests.post(
+        slack_channel, data = json.dumps(slack_payload), headers = {'Content-Type': 'application/json'}
+    )
 else:
     print("No rows have Use% >= 75.")
+
+# df -h | awk '$5+0 > 75 {print $0}'
+
      
 
